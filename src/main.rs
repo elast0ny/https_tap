0f4eac2f6df20ed7aa00e604b4e272232acce724
ElasT0ny <elast0ny00@gmail.com>
2020-09-17 16:49:03 -0400

    Added proper dns querying

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// You can run this example from the root of the mio repo:
-// cargo run --example tcp_server --features="os-poll tcp"
 use mio::event::Event;
 use mio::net::{TcpListener, TcpStream};
 use mio::Waker;
@@ -14,6 +12,8 @@ use std::sync::Arc;
 use env_logger::Env;
 use log::*;
 use structopt::StructOpt;
+use trust_dns_resolver::config::*;
+use trust_dns_resolver::Resolver;
 
 type Result<T> = std::result::Result<T, std::boxed::Box<dyn std::error::Error>>;
 
@@ -34,17 +34,24 @@ struct Args {
     /// Path to the private key file
     #[structopt(short, long)]
     key: PathBuf,
-
     /// The IP of the interface to bind to
     #[structopt(short, long, default_value = "0.0.0.0")]
     bind: String,
-    /// Port to listen on
+    /// Port to listen on and forward to
     #[structopt(short, long, default_value = "443")]
     port: u16,
-
     /// Where to replay the plaintext traffic
     #[structopt(short, long, default_value = "127.0.0.1:4433")]
     loopback: String,
+}
+
+struct Ctx {
+    connections: HashMap<Token, Client>,
+    remote_port: u16,
+    loopback_addr: String,
+    unique_id: usize,
+    loopback_id_queue: VecDeque<usize>,
+    resolver: Resolver,
 }
 
 const SERVER: usize = 0;
@@ -62,8 +69,6 @@ fn main() -> Result<()> {
     let addr = format!("{}:{}", args.bind, args.port).parse().unwrap();
     let mut listener = TcpListener::bind(addr)?;
     let mut loopback = TcpListener::bind(args.loopback.parse()?)?;
-    let mut unique_id = LOOPBACK;
-    let mut loopback_id_queue: VecDeque<usize> = VecDeque::new();
     // Create a poll instance.
     let mut poll = Poll::new()?;
     // Create storage for events.
@@ -73,7 +78,17 @@ fn main() -> Result<()> {
     poll.registry()
         .register(&mut loopback, Token(LOOPBACK), Interest::READABLE)?;
     info!("Waiting for connections !");
-    let mut connections: HashMap<Token, Client> = HashMap::new();
+    let mut res_opts = ResolverOpts::default();
+    res_opts.use_hosts_file = false;
+
+    let mut ctx = Ctx {
+        remote_port: args.port,
+        connections: HashMap::new(),
+        loopback_addr: args.loopback,
+        unique_id: LOOPBACK,
+        loopback_id_queue: VecDeque::new(),
+        resolver: Resolver::new(ResolverConfig::google(), res_opts).unwrap(),
+    };
 
     loop {
         poll.poll(&mut events, None)?;
@@ -99,10 +114,10 @@ fn main() -> Result<()> {
                         }
                     };
                     let id = Token(if is_loopback {
-                        loopback_id_queue.pop_front().unwrap()
+                        ctx.loopback_id_queue.pop_front().unwrap()
                     } else {
-                        unique_id += 1;
-                        unique_id
+                        ctx.unique_id += 1;
+                        ctx.unique_id
                     });
 
                     debug!("Accept peer {}", id.0);
@@ -117,7 +132,7 @@ fn main() -> Result<()> {
                         },
                     )?;
 
-                    connections.insert(
+                    ctx.connections.insert(
                         id,
                         Client {
                             tls: Box::new(rustls::ServerSession::new(&ssl_ctx)),
@@ -130,15 +145,9 @@ fn main() -> Result<()> {
                     );
                 },
                 cli_id => {
-                    if let Err(_e) = handle_client(
-                        poll.registry(),
-                        &mut connections,
-                        event,
-                        &mut unique_id,
-                        &mut loopback_id_queue,
-                    ) {
+                    if let Err(_e) = handle_client(poll.registry(), event, &mut ctx) {
                         let mut peer_id = 0;
-                        if let Some(mut cli) = connections.remove(&Token(cli_id)) {
+                        if let Some(mut cli) = ctx.connections.remove(&Token(cli_id)) {
                             let _ = poll.registry().deregister(&mut cli.socket);
                             if !cli.is_loopback {
                                 //warn!("[{}] Error, closing connection : {}", cli_id, e);
@@ -149,7 +158,7 @@ fn main() -> Result<()> {
                             let _ = cli.socket.shutdown(std::net::Shutdown::Both);
                         }
                         if peer_id != 0 {
-                            if let Some(mut cli) = connections.remove(&Token(peer_id)) {
+                            if let Some(mut cli) = ctx.connections.remove(&Token(peer_id)) {
                                 let _ = poll.registry().deregister(&mut cli.socket);
                                 if !cli.is_loopback {
                                     //warn!("Also closing connected peer {}", peer_id);
@@ -204,15 +213,9 @@ fn load_ssl_config(args: &Args) -> Result<Arc<rustls::ServerConfig>> {
     Ok(Arc::new(ssl_config))
 }
 
-fn handle_client(
-    registry: &Registry,
-    connections: &mut HashMap<Token, Client>,
-    event: &Event,
-    unique_id: &mut usize,
-    loopback_id_queue: &mut VecDeque<usize>,
-) -> Result<()> {
+fn handle_client(registry: &Registry, event: &Event, ctx: &mut Ctx) -> Result<()> {
     let cli_id = &event.token();
-    let cli = match connections.get_mut(cli_id) {
+    let cli = match ctx.connections.get_mut(cli_id) {
         Some(c) => c,
         None => return Err(From::from(format!("Unknown client {}", cli_id.0))),
     };
@@ -243,6 +246,7 @@ fn handle_client(
             }
         };
     }
+
     if event.is_readable() {
         while cli.tls.wants_read() {
             match cli.tls.read_tls(&mut cli.socket) {
@@ -298,6 +302,20 @@ fn handle_client(
             }
             let domain = domain.unwrap();
             let dns_name = webpki::DNSNameRef::try_from_ascii_str(domain).unwrap();
+
+            // Lookup the IP addresses associated with a name.
+            let response = ctx.resolver.lookup_ip(domain).unwrap();
+            let address = match response.iter().next() {
+                Some(a) if a.is_ipv4() => a.to_string(),
+                Some(a) if a.is_ipv6() => a.to_string(),
+                _ => {
+                    return Err(From::from(format!(
+                        "Unable to resolve IP for host : {}",
+                        domain
+                    )))
+                }
+            };
+
             // Create SSL context
             let mut config = rustls::ClientConfig::new();
             config
@@ -307,18 +325,18 @@ fn handle_client(
 
             //Connect to plaintext loopback
             info!("Connecting to loopback");
-            let mut loopback_sock = match TcpStream::connect("127.0.0.1:4433".parse()?) {
+            let mut loopback_sock = match TcpStream::connect(ctx.loopback_addr.parse()?) {
                 Ok(s) => s,
                 Err(e) => return Err(From::from(format!("Failed to connect to loopback : {}", e))),
             };
-            *unique_id += 1;
-            loopback_id = *unique_id;
-            *unique_id += 1;
-            let peer_loopback_id = *unique_id;
-            loopback_id_queue.push_back(peer_loopback_id);
+            ctx.unique_id += 1;
+            loopback_id = ctx.unique_id;
+            ctx.unique_id += 1;
+            let peer_loopback_id = ctx.unique_id;
+            ctx.loopback_id_queue.push_back(peer_loopback_id);
             registry.register(&mut loopback_sock, Token(loopback_id), Interest::READABLE)?;
             cli.loopback_id = loopback_id;
-            connections.insert(
+            ctx.connections.insert(
                 Token(loopback_id),
                 Client {
                     tls: Box::new(rustls::ClientSession::new(&config, dns_name)),
@@ -330,20 +348,21 @@ fn handle_client(
                 },
             );
             // Connect to real domain
-            info!("Connecting to {}", domain);
-            let mut peer_sock = match TcpStream::connect("192.168.2.222:443".parse()?) {
-                Ok(s) => s,
-                Err(e) => return Err(From::from(format!("Failed to connect to peer : {}", e))),
-            };
-            *unique_id += 1;
-            forward_id = *unique_id;
+            info!("Connecting to {} ({}:{})", domain, address, ctx.remote_port);
+            let mut peer_sock =
+                match TcpStream::connect(format!("{}:{}", address, ctx.remote_port).parse()?) {
+                    Ok(s) => s,
+                    Err(e) => return Err(From::from(format!("Failed to connect to peer : {}", e))),
+                };
+            ctx.unique_id += 1;
+            forward_id = ctx.unique_id;
             debug!("[{}] New peer {}", cli_id.0, forward_id);
             registry.register(
                 &mut peer_sock,
                 Token(forward_id),
                 Interest::READABLE.add(Interest::WRITABLE),
             )?;
-            connections.insert(
+            ctx.connections.insert(
                 Token(forward_id),
                 Client {
                     tls: Box::new(rustls::ClientSession::new(&config, dns_name)),
@@ -362,7 +381,7 @@ fn handle_client(
         }
 
         //Forward to loopback
-        if let Some(peer) = connections.get_mut(&Token(loopback_id)) {
+        if let Some(peer) = ctx.connections.get_mut(&Token(loopback_id)) {
             debug!("Forwarding to loopback {}", loopback_id);
             peer.remote_recv.extend(&forward_data);
             let waker = Waker::new(registry, Token(loopback_id))?;
@@ -370,7 +389,7 @@ fn handle_client(
         }
 
         //Forward to remote peer
-        if let Some(peer) = connections.get_mut(&Token(forward_id)) {
+        if let Some(peer) = ctx.connections.get_mut(&Token(forward_id)) {
             debug!("Forwarding to peer {}", forward_id);
             peer.remote_recv.extend(&forward_data);
             let waker = Waker::new(registry, Token(forward_id))?;
